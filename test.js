a9c2be5e3d31f0763f1220e19460d159d2ca6de3
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2018-10-26 02:33:50 +0700

    add test

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-'use strict';
-let test = require('ava');
-let { createElement: h } = require('react');
-let ReactTestRenderer = require('react-test-renderer');
-let useInputValue = require('./');
+"use strict";
+let test = require("ava");
+let { createElement: h } = require("react");
+let ReactTestRenderer = require("react-test-renderer");
+let useOnlineStatus = require("./");
 
 function render(val) {
   return ReactTestRenderer.create(val);
@@ -10,11 +10,11 @@ function render(val) {
 
 test(t => {
   function Component() {
-    let value = use...();
-    return h('div');
+    let value = useOnlineStatus();
+    return h("div", { value });
   }
 
   let input = render(h(Component));
 
-  t.is(input.toJSON().props.value, '...');
+  t.is(input.toJSON().props.value, true);
 });
